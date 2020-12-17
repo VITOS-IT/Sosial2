4509b4cea671ca629de3f8446d85137426e7d80f
VITOS-IT <vitos.iva@gmail.com>
2020-12-17 21:39:53 +0200

    56 lesson container component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,70 +1,54 @@
 import React from 'react'
 import s from './users.module.css'
-import * as axios from "axios";
 import altImg from '../../img/altAvatar.jpg'
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
+let Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = (pageNumber) =>{
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            });
-    }
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i<=pagesCount; i++){
-            pages.push(i);
-        }
-
-        return <div >
-            <div className={s.usersPage}>
+    return <div>
+        <div className={s.usersPage}>
             {pages.map(u => {
-               return <span className={this.props.currentPage === u && s.selectedPage}
-                            onClick={(e)=>{this.onPageChanged(u);}}>{u}</span>
+                return <span className={props.currentPage === u && s.selectedPage}
+                             onClick={() => {
+                                 props.onPageChanged(u);
+                             }}>{u}</span>
             })}
-            </div>
-            {
-                this.props.users.map(u => <div key={u.id} className={s.userContainer}>
-                    <div className={s.userFollowBlock}>
-                        <div>
-                            <img src={u.photos.small || altImg} alt="avatar image" className={s.userPhoto}/>
-                        </div>
-                        <div>
-                            {u.followed ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
-                                }} className={s.btnFollow}>Unfollow</button>
-                                : <button onClick={() => {
-                                    this.props.follow(u.id)
-                                }} className={s.btnFollow}>Follow</button>}
-
-                        </div>
-                    </div>
-                    <div className={s.userInfoBlock}>
-                        <div className={s.userInfoLeft}>
-                            <div>Name: {u.name}  </div>
-                            <div>Status: {u.status}  </div>
-                        </div>
-                        <div className={s.userInfoRight}>
-                            <div>Country:</div>
-                            <div>id: {u.id}  </div>
-                        </div>
-                    </div>
-                </div>)
-            }
         </div>
-    }
+        {
+            props.users.map(u => <div key={u.id} className={s.userContainer}>
+                <div className={s.userFollowBlock}>
+                    <div>
+                        <img src={u.photos.small || altImg} alt="avatar image" className={s.userPhoto}/>
+                    </div>
+                    <div>
+                        {u.followed ? <button onClick={() => {
+                                props.unfollow(u.id)
+                            }} className={s.btnFollow}>Unfollow</button>
+                            : <button onClick={() => {
+                                props.follow(u.id)
+                            }} className={s.btnFollow}>Follow</button>}
 
+                    </div>
+                </div>
+                <div className={s.userInfoBlock}>
+                    <div className={s.userInfoLeft}>
+                        <div>Name: {u.name}  </div>
+                        <div>Status: {u.status}  </div>
+                    </div>
+                    <div className={s.userInfoRight}>
+                        <div>Country:</div>
+                        <div>id: {u.id}  </div>
+                    </div>
+                </div>
+            </div>)
+        }
+    </div>
 }
 
 
